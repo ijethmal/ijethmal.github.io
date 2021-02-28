11c6079ed4697d1ff324e3f308226b7b070307bc
Isha <s10205038@connect.np.edu.sg>
2021-02-28 16:06:08 +0800

    changed pages

--- js/nav-scripts.js
+++ js/nav-scripts.js
@@ -1,9 +1,17 @@
 function revealBlog() {
     document.getElementById('blog').style.display = "block";
     document.getElementById('education').style.display = "none";
+    document.getElementById('onlineLinks').style.display = "none";
 }
 
 function revealEdu() {
     document.getElementById('education').style.display = "block";
     document.getElementById('blog').style.display = "none";
+    document.getElementById('onlineLinks').style.display = "none";
+}
+
+function revealOnline() {
+    document.getElementById('onlineLinks').style.display = "block";
+    document.getElementById('blog').style.display = "none";
+    document.getElementById('education').style.display = "none";
 }
