b95486e2a40d520193249531a8c0d1556122ec5f
Isha <s10205038@connect.np.edu.sg>
2021-03-09 18:26:43 +0800

    changes to existing pages, new 'about me' page for personal writeup

--- js/nav-scripts.js
+++ js/nav-scripts.js
@@ -2,16 +2,26 @@ function revealBlog() {
     document.getElementById('blog').style.display = "block";
     document.getElementById('education').style.display = "none";
     document.getElementById('onlineLinks').style.display = "none";
+    document.getElementById('writeup').style.display = "none";
 }
 
 function revealEdu() {
     document.getElementById('education').style.display = "block";
     document.getElementById('blog').style.display = "none";
     document.getElementById('onlineLinks').style.display = "none";
+    document.getElementById('writeup').style.display = "none";
 }
 
 function revealOnline() {
     document.getElementById('onlineLinks').style.display = "block";
+    document.getElementById('blog').style.display = "none";
+    document.getElementById('education').style.display = "none";
+    document.getElementById('writeup').style.display = "none";
+}
+
+function revealWriteup() {
+    document.getElementById('writeup').style.display = "block";
+    document.getElementById('onlineLinks').style.display = "none";
     document.getElementById('blog').style.display = "none";
     document.getElementById('education').style.display = "none";
 }
